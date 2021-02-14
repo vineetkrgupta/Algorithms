b817c4bfff1f6b1b0632e2d2f9618d60e431f165
vineetkrgupta <vineet.kumar2019@vitbhopal.ac.in>
2021-02-15 04:19:49 +0530

    added

---
+++ Algo Classical Problems/kruskal_algorithm.py
@@ -0,0 +1,15 @@
+"""
+Notes:
+    - Given connected graph G with positive edge weights, find a min weight set of edges that 
+      connects all of the vertices.
+    - A single graph can have multiple spanning trees.
+    - MST weight is equal or less than the weights of other spanning trees.
+    - MST has V-1 edges
+
+Applications:
+    - Networks
+    - Approximation algorithms (travelling salesman)
+
+References:
+    - https://www.cs.princeton.edu/courses/archive/spr07/cos226/lectures/mst.pdf 
+"""
