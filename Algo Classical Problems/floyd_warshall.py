8733a78ed3c826fa5df5f61961a81d8bf2eedf86
vineetkrgupta <vineet.kumar2019@vitbhopal.ac.in>
2021-03-08 04:13:12 +0530

    added

---
+++ Algo Classical Problems/floyd_warshall.py
@@ -0,0 +1,20 @@
+"""
+Notes:
+    - Introduced in 1962
+    - Algorithm to find the shortest path in a weighted graph with positive and negative weights.
+    - No Negative cycles.
+    - Uses Dynamic Programming.
+    - O(V ^ 3) runtime
+
+Applications:
+    - Finding trasitive closure in a directed graph.
+    - Inversion of real matrices.
+    - Optimal routing.
+    - Fast computation of pathfinder networks.
+    - Computing similarity b/w graphs.
+
+References:
+    - https://en.wikipedia.org/wiki/Floyd–Warshall_algorithm
+"""
+
+
