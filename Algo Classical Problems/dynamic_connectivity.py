26f476fb55218e908dcc098cc1a1b7f34ffa6ef7
vineetkrgupta <vineet.kumar2019@vitbhopal.ac.in>
2021-03-11 04:11:25 +0530

    added

---
+++ Algo Classical Problems/dynamic_connectivity.py
@@ -0,0 +1,22 @@
+"""
+Notes:
+    - Given a set of N objects, find a path connecting objects.
+
+Applications:
+    - Dynamic Connectivity.
+    - Percolation Problem.
+    - Friends in a social network.
+    - Pixels in a photo.
+    - Games.
+
+ToDo:
+    - Implementation
+    - Tests
+
+References:
+    - https://python-algorithms.readthedocs.io/en/stable/python_algorithms.basic.html#module-python_algorithms.basic.union_find)
+"""
+
+
+
+
